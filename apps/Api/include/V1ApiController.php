103eb446cacc64f7431999e360dbea352a8eb7fa
Trong Hieu Luu <mylifeisskidrow@gmail.com>
2013-05-25 09:20:48 +0700

    - Fixed some bugs

--- apps/Api/include/V1ApiController.php
+++ apps/Api/include/V1ApiController.php
@@ -5,30 +5,4 @@ class V1ApiController extends BaseApiController {
     public function beforeExecute() {
         parent::beforeExecute();
     }
-
-
-
-    /**
-     * @param Account $acc
-     * @return array
-     */
-    public function getAccountObject(\Account $acc) {
-        return $acc->getAttributes($this->accResponseFields);
-    }
-
-    /**
-     * @param Transaction $t
-     * @return array
-     */
-    public function getTransactionObject(\Transaction $t) {
-        $attr = $t->getAttributes($this->transactionResponseFields);
-        if ($t->relate_to) {
-            $attr['relate_to'] = \Transaction::retrieveById($t->relate_to)->uk;
-        } else {
-            $attr['relate_to'] = '';
-        }
-        $attr['from_acc_uk'] = \Account::retrieveById($t->from_acc)->uk;
-        $attr['to_acc_uk'] = \Account::retrieveById($t->to_acc)->uk;
-        return $attr;
-    }
 }
