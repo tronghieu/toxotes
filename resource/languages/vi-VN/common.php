d5520951f8975f87ff7aaeec2b8a160a15146063
Trong Hieu Luu <luutronghieu@alimama.vn>
2013-09-19 07:27:50 +0700

    Add Language

--- resource/languages/vi-VN/common.php
+++ resource/languages/vi-VN/common.php
@@ -1,4 +1,5 @@
 <?php
 return array(
     'messages' => array(
-        'password not match!' => 'mật khẩu xác nhận không khớp!'));
+        'password not match!' => 'mật khẩu xác nhận không khớp!',
+        'View more' => 'Xem thêm',));
